2a33cd7ba71f134a6190fd0f8e679c069f5b7d57
brong <sajidbrong@gmail.com>
2021-04-15 22:18:43 +0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import {FaGulp} from 'react-icons/fa';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+
+    <p>
+
+
+    </p>
+  </Layout>
+)
+
+export default IndexPage
